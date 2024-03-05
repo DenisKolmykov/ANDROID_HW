61ad78ee8b65c9c5ffdd152a367843728e833025
DenisKolmykov <soary@yandex.ru>
2024-03-05 11:24:28 +0300

    lesson08 - hw (animation)

---
+++ android_hw08_quiz_relise_animation/app/src/main/java/com/example/android_hw08_quiz_relize_animation/questions/Question.kt
@@ -0,0 +1,11 @@
+package com.example.android_hw08_quiz_relize_animation.questions
+
+class Question(val question: String,
+               val answer1: String,
+               val answer2: String,
+               val answer3: String,
+               val answer4: String,
+               val correctAnswer: Int,
+               val points: Int) {
+
+}
